9351ba4a0a164959bf0e67ce4cc79cf913c04892
cleversamerr <thedev.samer@gmail.com>
2023-09-08 16:33:36 +0300

    initial commit with starter files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Home = () => {
+  return (
+    <Container>
+      <Heading>Home</Heading>
+    </Container>
+  );
+};
+
+const Container = styled.main``;
+
+const Heading = styled.h1``;
+
+export default Home;
